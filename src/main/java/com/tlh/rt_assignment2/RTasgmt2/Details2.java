925bc55227424e37c14b1687b5f0d275488e3113
TEE LI HONG <31922731+TEELIHONG@users.noreply.github.com>
2017-11-01 19:29:39 +0800

    Update Details2.java

--- src/main/java/com/tlh/rt_assignment2/RTasgmt2/Details2.java
+++ src/main/java/com/tlh/rt_assignment2/RTasgmt2/Details2.java
@@ -4,7 +4,6 @@
 //Task: #Assignment2
 //Matrik: #240403
 //Name: #Tee Li Hong
-
 package com.tlh.rt_assignment2.RTasgmt2;
 
 import static com.tlh.rt_assignment2.RTasgmt2.RTasgmt2.KEYWORDS;
@@ -24,24 +23,26 @@ import java.io.Reader;
 import java.nio.charset.StandardCharsets;
 import org.apache.poi.ss.usermodel.Row;
 
-
 public class Details2 {
-    
-    public static int[] keywordcount1 = new int[55];
-    
-public void details2() throws IOException {
+
+    static final int[] keywordcount1 = new int[55];
+
+    public void details2() throws IOException {
         int empty1 = 0;
         int commentsNo1 = 0;
-        int actualLOC1 = 0;
+        int actualLOC1;
         int linenumber1 = 0;
         int totalkeyword1 = 0;
-        int total1 = 0;
+        int total1;
 
         try {
-            File file = new File(PATH + "\\MyThread2.java");
+            File myfile;
+            myfile = new File(PATH + File.separator + "MyThread2.java");
 
-            BufferedReader b = new BufferedReader(new FileReader(file));
-            String readLine = "";
+            Reader reader = new InputStreamReader(new FileInputStream(myfile), StandardCharsets.UTF_8);
+            BufferedReader b = new BufferedReader(new FileReader(myfile));
+
+            String readLine;
 
             Row row9 = sheet.createRow(8);
 
@@ -85,9 +86,9 @@ public void details2() throws IOException {
                 }
 
                 for (int count = 0; count < KEYWORDS.length; count++) {
-                    if (keywordcount1[count] != 0) {
-                        //System.out.println(KEYWORDS[count] + ":" + keywordcount[count]);
-                    }
+                    /*if (keywordcount1[count] != 0) {
+                        System.out.println(KEYWORDS[count] + ":" + keywordcount[count]); //display the used keywords list
+                    }*/
                     totalkeyword1 += keywordcount1[count];
                 }
                 int x = 5;
@@ -104,23 +105,20 @@ public void details2() throws IOException {
                 //System.out.println("total:" + totalkeyword);
                 //System.out.println("Total number of empty lines : " + empty);
                 //System.out.println("Total number of comments : " + commentsNo);
-                if (file.exists()) {
+                if (myfile.exists()) {
 
-                    FileReader fr = new FileReader(file);
-                    Reader reader = new InputStreamReader(new FileInputStream(file), StandardCharsets.UTF_8);
-                    LineNumberReader lnr = new LineNumberReader(reader);
-
-                    while (lnr.readLine() != null) {
-                        linenumber1++;
+                    try (LineNumberReader lnr = new LineNumberReader(reader)) {
+                        while (lnr.readLine() != null) {
+                            linenumber1++;
+                        }
+                        row9.createCell(1).setCellValue(linenumber1);
+                        //System.out.println("Total number of lines : " + linenumber);
+                        actualLOC1 = linenumber1 - empty1 - commentsNo1;
+                        row9.createCell(4).setCellValue(actualLOC1);
+                        //System.out.println("Actual LOC = " + actualLOC);
+                        total1 = totalkeyword1 + actualLOC1;
+                        row9.createCell(12).setCellValue(total1);
                     }
-                    row9.createCell(1).setCellValue(linenumber1);
-                    //System.out.println("Total number of lines : " + linenumber);
-                    actualLOC1 = linenumber1 - empty1 - commentsNo1;
-                    row9.createCell(4).setCellValue(actualLOC1);
-                    //System.out.println("Actual LOC = " + actualLOC);
-                    total1 = totalkeyword1 + actualLOC1;
-                    row9.createCell(12).setCellValue(total1);
-                    lnr.close();
 
                 } else {
                     //System.out.println("File does not exists!");
@@ -131,8 +129,8 @@ public void details2() throws IOException {
 
         } catch (IOException e) {
         }
-        FileOutputStream fileOut = new FileOutputStream(filename);
-        workbook.write(fileOut);
-        fileOut.close();
+        try (FileOutputStream fileOut = new FileOutputStream(filename)) {
+            workbook.write(fileOut);
+        }
     }
 }
